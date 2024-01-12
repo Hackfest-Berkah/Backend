645571b6017c212b872bf152b7c9d2bdc090769c
Bagas-AP <bantarino4@gmail.com>
2024-01-12 22:36:25 +0700

    edited markdown file for submission stage 2

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Backend
+
+Documentation for this repository: [Postman](https://documenter.getpostman.com/view/22317100/2s9YsJBsjE)
+
+Requirements:
+1. Go 1.19 or higher
+2. Google Cloud Platform Credentials
+3. Supabase Credentials
+
+Step:
+1. Clone this repository
+2. cp .env.example .env
+3. Fill the .env file with your own and google cloud platform credentials
+4. go mod tidy
+5. go run main.go
+
+Screenshots:
+1. Google Cloud Platform
+
+   ![Alt Text](https://storage.googleapis.com/image-kiri/test-files/gcp.png)
+
+2. Supabase
+
+   ![Alt Text](https://storage.googleapis.com/image-kiri/test-files/hackfest.png)
+
+3. Postman
+
+   ![Alt Text](https://storage.googleapis.com/image-kiri/test-files/postman.png)
--- controller/qr.go
+++ controller/qr.go
@@ -12,7 +12,7 @@ import (
 func QR(db *gorm.DB, q *gin.Engine) {
 	r := q.Group("/api/v1")
 
-	r.GET("/qr", middleware.Authorization(), func(c *gin.Context) {
+	r.GET("/qr/:userID", middleware.Authorization(), func(c *gin.Context) {
 		ID, _ := c.Get("id")
 
 		var user model.User
@@ -23,5 +23,29 @@ func QR(db *gorm.DB, q *gin.Engine) {
 		}
 
 		utils.HttpRespSuccess(c, http.StatusOK, "Success get image", user.QRCode)
+	})
+
+	r.POST("/qr/scan/:amount", middleware.Authorization(), func(c *gin.Context) {
+		ID, _ := c.Get("id")
+
+		amountStr := c.Param("amount")
+		amount := utils.StringToFloat(amountStr, c)
+
+		var user model.User
+
+		if err := db.Where("id = ?", ID).First(&user).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+			return
+		}
+
+		user.KiriBalance += amount
+		user.KiriPoint += amount
+
+		if err := db.Save(&user).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		utils.HttpRespSuccess(c, http.StatusOK, "Success scan QR", nil)
 	})
 }
